1b44876e0a23f84037a35dfd7ff995952df07376
vadim.turkov <vadim.turkov@RUS-VTURKOV>
2018-09-17 09:32:10 +0300

    js: add solution to highest and lowest kata

---
+++ js/7kyu/highest-and-lowest/highest-and-lowest.js
@@ -0,0 +1,7 @@
+// Solution to CodeWars kata: Highest and lowest
+// https://www.codewars.com/kata//highest-and-lowest
+
+function highAndLow(str) {
+    const numbers = str.split(' ').sort((x, y) => x - y);
+    return `${numbers[numbers.length - 1]} ${numbers[0]}`;
+  }
