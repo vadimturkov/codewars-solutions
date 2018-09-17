41844036ff2c0043636e3fcfae4e3969db88b8df
vadim.turkov <vadim.turkov@RUS-VTURKOV>
2018-09-17 09:12:12 +0300

    js: add solution to vowel-count kata

---
+++ js/7kyu/vowel-count/vowel-count.js
@@ -0,0 +1,12 @@
+// Solution to CodeWars kata: Vowel count
+// https://www.codewars.com/kata/vowel-count
+
+function getCount(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    return str.split('').reduce((count, char) => {
+      if (vowels.includes(char)) {
+        count++;
+      }
+      return count;
+    }, 0);
+  }
