a98afcc6089ccec5060a7b188e0837fa8fba6cb3
vadim.turkov <vadim.turkov@RUS-VTURKOV>
2018-09-18 10:56:36 +0300

    sql: add solution to SQL Basics: Simple WHERE and ORDER BY kata

---
+++ sql/8kyu/sql-basics-simple-where-and-order-by/sql-basics-simple-where-and-order-by.sql
@@ -0,0 +1,7 @@
+-- Solution to CodeWars kata: SQL Basics: Simple WHERE and ORDER BY
+-- https://www.codewars.com/kata/sql-basics-simple-where-and-order-by/
+
+SELECT *
+FROM people
+WHERE age > 50
+ORDER BY age DESC;
