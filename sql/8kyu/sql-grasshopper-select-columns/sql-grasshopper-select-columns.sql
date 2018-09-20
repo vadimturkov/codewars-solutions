de43d7cebef4cac32c3383b0acaea99845aea197
vadim.turkov <vadim.turkov@gmail.com>
2018-09-20 11:40:52 +0300

    sql: add solution to SQL Grasshopper: Select Columns kata

---
+++ sql/8kyu/sql-grasshopper-select-columns/sql-grasshopper-select-columns.sql
@@ -0,0 +1,5 @@
+-- Solution to CodeWars kata: SQL Grasshopper: Select Columns
+-- https://www.codewars.com/kata/sql-grasshopper-select-columns/
+
+SELECT custid, custname, custstate 
+FROM customers
