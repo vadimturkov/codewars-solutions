f4eebe23a483c2da9d38125dc79437e36a040260
vadim.turkov <vadim.turkov@gmail.com>
2018-09-21 11:30:11 +0300

    sql: add solution to Adults only (SQL for Beginners #1) kata

---
+++ sql/8kyu/adults-only-sql-for-beginners-number-1/adults-only-sql-for-beginners-number-1.sql
@@ -0,0 +1,6 @@
+-- Solution to CodeWars kata: Adults only (SQL for Beginners #1)
+-- https://www.codewars.com/kata/adults-only-sql-for-beginners-number-1/
+
+SELECT name, age
+FROM users
+WHERE age >= 18
