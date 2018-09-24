4eafba3695f8f8843a8649bec0fccbd178889a98
vadim.turkov <vadim.turkov@gmail.com>
2018-09-24 11:12:30 +0300

    sql: add solution to Easy SQL: Rounding Decimals kata

---
+++ sql/8kyu/easy-sql-rounding-decimals/easy-sql-rounding-decimals.sql
@@ -0,0 +1,5 @@
+-- Solution to CodeWars kata: Easy SQL: Rounding Decimals
+-- https://www.codewars.com/kata/easy-sql-rounding-decimals/
+
+SELECT floor(number1) AS number1, ceil(number2) AS number2    
+FROM decimals
