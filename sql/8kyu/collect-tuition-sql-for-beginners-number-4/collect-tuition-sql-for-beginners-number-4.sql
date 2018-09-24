3052b2057350395da1a3bd24648ec30d6394d4a2
vadim.turkov <vadim.turkov@gmail.com>
2018-09-24 16:29:36 +0300

    sql: add solution to Collect Tuition (SQL for Beginners #4) kata

---
+++ sql/8kyu/collect-tuition-sql-for-beginners-number-4/collect-tuition-sql-for-beginners-number-4.sql
@@ -0,0 +1,6 @@
+-- Solution to CodeWars kata: Collect Tuition (SQL for Beginners #4)
+-- https://www.codewars.com/kata/collect-tuition-sql-for-beginners-number-4/
+
+SELECT * 
+FROM students 
+WHERE NOT tuition_received
