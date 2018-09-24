0ed2bf0b33ed77361604eea02f3351dff7a9b32c
vadim.turkov <vadim.turkov@gmail.com>
2018-09-24 18:16:44 +0300

    sql: add solution to SQL Basics: Simple MIN / MAX kata

---
+++ sql/8kyu/sql-basics-simple-min-slash-max/sql-basics-simple-min-slash-max.sql
@@ -0,0 +1,5 @@
+-- Solution to CodeWars kata: SQL Basics: Simple MIN / MAX
+-- https://www.codewars.com/kata/sql-basics-simple-min-slash-max/
+
+SELECT min(age) AS age_min, max(age) AS age_max
+FROM people
