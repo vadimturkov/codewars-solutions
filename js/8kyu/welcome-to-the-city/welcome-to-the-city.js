519769b407b48dada0060f0b319c0208b09a3a9f
vadim.turkov <vadim.turkov@gmail.com>
2018-09-24 11:23:18 +0300

    js: add solution to Welcome to the City kata

---
+++ js/8kyu/welcome-to-the-city/welcome-to-the-city.js
@@ -0,0 +1,6 @@
+// Solution to CodeWars kata: Welcome to the City
+// https://www.codewars.com/kata/welcome-to-the-city/
+
+function sayHello(name, city, state) {
+    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+}
