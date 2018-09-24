cf968748f95584bd6cbb0f9784cbd52b4c6fa46e
vadim.turkov <vadim.turkov@gmail.com>
2018-09-24 16:24:41 +0300

    sql: add solution to Register for the Party (SQL for Beginners #3) kata

---
+++ sql/8kyu/register-for-the-party-sql-for-beginners-number-3/register-for-the-party-sql-for-beginners-number-3.sql
@@ -0,0 +1,6 @@
+-- Solution to CodeWars kata: Register for the Party (SQL for Beginners #3)
+-- https://www.codewars.com/kata/register-for-the-party-sql-for-beginners-number-3/
+
+INSERT INTO participants VALUES ('Vadim', 28, true);
+
+SELECT * FROM participants;
