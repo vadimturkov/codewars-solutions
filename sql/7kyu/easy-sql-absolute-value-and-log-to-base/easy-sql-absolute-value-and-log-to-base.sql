b1b81fe6def42c0f40d4d3b6d3d49a1a21e972b2
vadimturkov <vadim.turkov@gmail.com>
2018-11-29 12:14:58 +0300

    sql: add solution to kata Easy SQL: Absolute Value and Log to Base

---
+++ sql/7kyu/easy-sql-absolute-value-and-log-to-base/easy-sql-absolute-value-and-log-to-base.sql
@@ -0,0 +1,5 @@
+-- Solution to CodeWars kata: Easy SQL: Absolute Value and Log to Base
+-- https://www.codewars.com/kata/easy-sql-absolute-value-and-log-to-base/
+
+SELECT abs(number1) AS abs, log(64, number2) AS log
+FROM decimals
