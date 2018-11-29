29506d90b7b877ebe17cf7d6ec0ca9b0f4a29f0d
vadimturkov <vadim.turkov@gmail.com>
2018-11-29 11:58:34 +0300

    js: add solution to kata Breaking chocolate problem

---
+++ js/7kyu/breaking-chocolate-problem/breaking-chocolate-problem.js
@@ -0,0 +1,6 @@
+// Solution to CodeWars kata: Breaking chocolate problem
+// https://www.codewars.com/kata/breaking-chocolate-problem/
+
+function breakChocolate(n,m) {
+  return n >= 1 && m >= 1 ? n * m - 1 : 0;
+}
