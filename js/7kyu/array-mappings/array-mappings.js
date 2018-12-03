6548d50b5b8fa5e8920a4d398e70b433e34864b9
Vadim Turkov <vadim.turkov@gmail.com>
2018-12-03 10:54:45 +0300

    js: add solution to kata Array Mappings

---
+++ js/7kyu/array-mappings/array-mappings.js
@@ -0,0 +1,9 @@
+// Solution to CodeWars kata: Array Mappings
+// https://www.codewars.com/kata/array-mappings/
+
+Array.prototype.map = function(fn) { 
+    return this.reduce((result, value) => {
+      result.push(fn(value));
+      return result;
+    }, []);
+  };
