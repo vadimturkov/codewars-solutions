820904551fbdf3cb81242f4ba6434e7dc284f912
Vadim Turkov <vadim.turkov@gmail.com>
2018-12-03 10:41:38 +0300

    js: add solution to kata Minimum Steps (Array Series #6)

---
+++ js/7kyu/minimum-steps-array-series-number-6/minimum-steps-array-series-number-6.js
@@ -0,0 +1,12 @@
+// Solution to CodeWars kata: Minimum Steps (Array Series #6)
+// https://www.codewars.com/kata/minimum-steps-array-series-number-6/
+
+function minimumSteps(numbers, value) {
+    const sorted = numbers.slice().sort((x, y) => x - y);
+    for(let i = 0, sum = 0; i < numbers.length; i++) {
+      sum += sorted[i];
+      if (sum >= value) {
+        return i;
+      }
+    }
+  }
